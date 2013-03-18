922acf869adde61d7b2e65836ab9978303bd71cd
Andreas Stephanides <andis@fetsitetest.fet.htu.tuwien.ac.at>
2013-03-18 21:06:32 +0100

    toolbar_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,12 @@ module ApplicationHelper
     current_url({:locale=>target_locale}) .sub "/"+I18n.locale.to_s+"/", "/"+target_locale.to_s+"/"
    
   end
-
+  def toolbar_html(elemente)
+    html = ""
+    	elemente.each do |e| 
+	html =html + e + " | "
+	end 
+    #html= html + "</ul>"
+    raw(html)
+  end
 end
