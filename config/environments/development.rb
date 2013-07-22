70be1adfea3e4ddd6e14ba59285d9fc3109bbf65
Andreas Stephanides <andreas.stephanides@gmail.com>
2013-07-22 15:43:58 +0200

    FIX: glonass hardcoded in application_controller and config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Fetsite::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = { :host => 'glonass.htu.tuwien.ac.at' }
+config.action_mailer.default_url_options = {} #  :host => 'glonass.htu.tuwien.ac.at' }
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
