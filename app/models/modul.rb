4aaaa286f3a8cc5dbe6efcac50acccc2a1aae8ff
Thomas Blazek <wtblazek@gmail.com>
2013-07-24 21:47:57 +0200

    Saveguaring Modul und LVA

--- app/models/modul.rb
+++ app/models/modul.rb
@@ -16,6 +16,8 @@ class Modul < ActiveRecord::Base
   has_and_belongs_to_many :lvas
   has_and_belongs_to_many :modulgruppen
   
+  validates :modulgruppen, :presence=>true
+  validates_presence_of :name
   translates :desc,:depend,:name, :versioning =>true, :fallbacks_for_empty_translations => true
 
 end
