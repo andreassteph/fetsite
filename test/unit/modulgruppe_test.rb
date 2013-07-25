163048a162957844d235288a49c12447973be089
Andreas Stephanides <andreas.stephanides@gmail.com>
2013-07-25 14:45:29 +0200

    Testen eines Tests für  Modulgruppe

--- test/unit/modulgruppe_test.rb
+++ test/unit/modulgruppe_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ModulgruppeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "should not be valid without name" do
+     mg=Modulgruppe.new();
+     mg.should_not be_valid;
+   end
 end
