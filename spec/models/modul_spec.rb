15a9b4fbe785df756c25a3d1daaf3200e876e825
Thomas Blazek <wtblazek@gmail.com>
2013-08-05 11:12:07 +0200

    Modul sollte keine Abhängigkeit von Modulgruppe haben

--- spec/models/modul_spec.rb
+++ spec/models/modul_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Modul do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be valid without name" do
+    m = FactoryGirl.build(:modul)
+    m.should_not be_valid
+    m.should have(1).errors_on(:name)
+  end
+  it "should be valid with name" do
+    m = FactoryGirl.build(:modul)
+    m.name = "Grundlagen"
+    m.should be_valid
+  end
 end
