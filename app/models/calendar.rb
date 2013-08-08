ae75459d58a14998e9e115be850537f99855140f
Andreas Stephanides <andis@fet.at>
2013-08-08 09:08:43 +0200

    Relationship Calentry,Calendar

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,3 +1,4 @@
 class Calendar < ActiveRecord::Base
   attr_accessible :name, :public
+  has_and_belongs_to_many :calentries
 end
--- app/models/calentry.rb
+++ app/models/calentry.rb
@@ -1,4 +1,10 @@
 class Calentry < ActiveRecord::Base
-  attr_accessible :ende, :start, :summary, :typ
-  
+  attr_accessible :ende, :start, :summary, :typ,:calendar_ids
+  has_and_belongs_to_many :calendar
+  def start_time
+    start
+  end
+  def name
+	summary
+  end
 end
