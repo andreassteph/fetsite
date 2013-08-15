9fe01afbaa5b108ecc7c7450f168b3f9ac2ea74e
Andreas Stephanides <andis@fetsitetest.fet.htu.tuwien.ac.at>
2013-08-15 12:06:28 +0200

    remove schema.rb and migration

--- db/migrate/20130812170437_add_themaid_to_thema_translations.rb
+++
@@ -1,6 +0,0 @@
-class AddThemaidToThemaTranslations < ActiveRecord::Migration
-  def change
-    add_column :thema_translations, :theman_id, :integer
-    remove_column :thema_translations, :themen_id, :integer
-  end
-end
