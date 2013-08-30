9f2c184e4304f80eb75c2e9f861bfb79e6227234
Andreas Stephanides <andreas.stephanides@gmail.com>
2013-08-30 23:28:27 +0200

    datepicker_input

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,23 +1,28 @@
 class DatepickerInput < FormtasticBootstrap::Inputs::StringInput
 	def input_html_options
-		super.merge(:class => "input-append date")
+		super
+		#super.merge(:class => "input-append date")
 	end
 	def html_options
-		super.merge(:default => Date.today)
+		super
+		#super.merge(:default => Date.today)
 	end
 	def wrapper_html_options
-		super.merge(:class=>"datepicker",'date-date-format'.to_sym=>"%d.%m.%Y")
+		super.merge(:class=>"")
+		#super.merge(:class=>"datepicker",'date-date-format'.to_sym=>"%d.%m.%Y")
 	end
 	def controls_wrapper_html_options
-		super.merge(:class=> "controls", 'date-date'.to_sym =>Time.now.to_date.to_s)
+	super.merge(:class=> "datepicker date input-append", 'data-date'.to_sym =>I18n.l(Date.today()).to_s, 'data-date-format'.to_sym=>I18n.t('date.formats.default-picker'))
 	end
-	#def to_html
-	#	bootstrap_wrapping do
-     #       builder.text_field(method, input_html_options)
-	#	end
-	#end
+	def to_html
+		bootstrap_wrapping do
+           	builder.text_field(method, input_html_options) +
+	   	'<span class="add-on" ><i class="icon-th"></i></span>'.html_safe()
+	end
+	end
 	def options
-		super#d.merge(:class=>"datepicker")
+		super
+#d.merge(:class=>"datepicker")
 		#super.merge(:append_content=>'<span class="add-on input-append"
 		#super.merge(:append_content=>'<span class="add-on" ><i class="icon-th"></i></span>')
 	end
