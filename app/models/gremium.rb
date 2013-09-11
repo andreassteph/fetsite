09c182048c941bd67436a74b14b541cca0eb4ed8
Andreas Stephanides <andreas.stephanides@gmail.com>
2013-09-11 20:08:08 +0200

    kommentare für gremium und calentry

--- app/models/gremium.rb
+++ app/models/gremium.rb
@@ -12,16 +12,15 @@
 #
 
 class Gremium < ActiveRecord::Base
-  TYPEN={1=>"offiziell", 2=>"offiziell-temporär", 3 => "inoffiziell",4=>"inoffiziell-tempo"}
-  TAPS_TYPEN={1=>true,2=>false,3=>true,4=>false}
-  GESCHLECHT={0=>"saechlich", 1 => "maennlich", 2 => "weiblich"}
-  ART2FALL={0=>"des", 1=>"des",2=>"der"}
+  TYPEN={1=>"offiziell", 2=>"offiziell-temporär", 3 => "inoffiziell",4=>"inoffiziell-tempo"} # Kategorien, im Wesentlichen wichtig für Listung oder nicht Listung
+  GESCHLECHT={0=>"saechlich", 1 => "maennlich", 2 => "weiblich"} # Geschlecht des Gremiums zur richtige Deklination
+  ART2FALL={0=>"des", 1=>"des",2=>"der"} # Artikel 2.Fall abhängig vom Geschlecht
   attr_accessible :desc, :name, :typ, :geschlecht,:thema_id
-  has_many :memberships
-  belongs_to :thema
-  scope :tabs, -> { where(:typ => [1,3]).order(:typ).order(:name) }
-  
-  def fall2
+  has_many :memberships # Mitgliedschaften bei dem Gremium
+  belongs_to :thema # Gehört zu einem Thema
+  scope :tabs, -> { where(:typ => [1,3]).order(:typ).order(:name) } # Gremien die in Tabs angezeigt werden (Alle Anderen nur in der Liste
+  # Gremium im 2. Fall für die Konstruktion "Mitglied des ... / der ... " 
+  def fall2 
     Gremium::ART2FALL[self.geschlecht.to_i].to_s+" "+ self.name.to_s+ ((self.geschlecht.to_i==1||self.geschlecht.to_i==0)? "s":"") 
   end
   
