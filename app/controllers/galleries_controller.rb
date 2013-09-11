77ee994edba98485473c88fa9817bdb8ee8a8e29
HausdorffHimself <andreas@X220.(none)>
2013-09-12 00:58:33 +0200

    NEW: Pictures per Site Feature + Pagination + improved SQL query

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -18,9 +18,21 @@ class GalleriesController < ApplicationController
   # GET /galleries/1.json
   def show
     @gallery = Gallery.find(params[:id])
+
+    @pppage_array = [ 5 , 50 , 100 ] #defines number & size of picture chunks
+    @pppage = 0 #starting index of pppage_array
+    
+    if params[:pppage].to_i <= 2 && params[:pppage].to_i >= 0
+      @pppage = params[:pppage].to_i
+    end
+      
+    @page = params[:page].nil? ? 1 : params[:page].to_i
+    @fotos = Foto.where(:gallery_id => params[:id]).limit(@pppage_array[@pppage]).offset(@pppage_array[@pppage]*(@page-1))
+    @pages = (Foto.where(:gallery_id => params[:id]).count/5)+1
     @toolbar_elements << {:hicon=>'icon-plus', :text=> "NewFoto", :path=>new_gallery_foto_path(@gallery)}
     @toolbar_elements << {:hicon=>'icon-pencil', :text => I18n.t('common.edit'), :path=>edit_gallery_path(@gallery)}
     @toolbar_elements << {:text=>"Back", :path=>galleries_path()}
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @gallery }
