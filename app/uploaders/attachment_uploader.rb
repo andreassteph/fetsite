c52e5192c019e47f32db0f476a022305df311182
Andreas Stephanides <andis@fet.at>
2014-07-18 01:03:23 +0530

    upfix

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -59,8 +59,6 @@ end
   version :resized, :if=>:image? do
     process :resize_to_fit => [1024,1024]
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
