6ee1468ce3f54c32f01fb21653e3350e0ade83e5
Andreas Stephanides <andreas.stephanides@gmail.com>
2014-10-04 12:09:56 +0200

    removecomments

--- app/models/beispiel.rb
+++ app/models/beispiel.rb
@@ -16,7 +16,7 @@ class Beispiel < ActiveRecord::Base
   attr_accessible :desc, :name, :lva_id, :beispieldatei, :beispieldatei_cache, :datum
   acts_as_votable
   belongs_to :lva
-  include IsCommentable
+
   mount_uploader :beispieldatei, AttachmentUploader
   validates :beispieldatei, :presence => true
   validates :name, :presence => true
