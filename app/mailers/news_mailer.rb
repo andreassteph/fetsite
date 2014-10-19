6a2be58a73903b8dfd22ef4d751ca320e847fe00
Andreas Stephanides <andreas.stephanides@gmail.com>
2014-10-19 22:16:34 +0200

    default host für mailer

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,8 +1,8 @@
 class NewsMailer < ActionMailer::Base
   default from: "salzamt@fet.at"
-  def neuigkeit_mail(email, neuigkeit_id,host)
+  def neuigkeit_mail(email, neuigkeit_id)
     @neuigkeit= Neuigkeit.find(neuigkeit_id)
-    @host=host
-    mail(to: "andis@fet.at", subject: @neuigkeit.title)
+    
+    mail(to: email, subject: @neuigkeit.title)
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Fetsite::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options={:host=>"glonass.htu.tuwien.ac.at"}
   # Enable threaded mode
   # config.threadsafe!
 
