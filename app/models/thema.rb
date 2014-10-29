2855b72bb6f92f40088fa1ed118a97386f1d141b
Andreas Stephanides <andreas.stephanides@gmail.com>
2014-10-29 13:44:17 +0100

    fix public thema

--- app/models/thema.rb
+++ app/models/thema.rb
@@ -21,9 +21,9 @@ include Rails.application.routes.url_helpers
   validates :themengruppe, :presence => true
   validates :title, :presence => true
   validates :text, :presence => true
-  scope :public, where(:isdraft=>false).includes(:themengruppe).where("themengruppen.public")
-  default_scope order(:priority).reverse_order
-  scope :search, ->(query) {where("text like ? or title like ?", "%#{query}%", "%#{query}%")}
+  scope :public, where(:isdraft=>false).includes(:themengruppe).where("themengruppen.public"=>true)
+  default_scope order("themen.priority").reverse_order
+  scope :search, ->(query) {where("themen.text like ? or themen.title like ?", "%#{query}%", "%#{query}%")}
   scope :outdated, -> {includes(:translations).where("thema_translations.updated_at<?",2.month.ago).where("thema_translations.locale"=>I18n.t.locale)
 }
   translates :title,:text, :versioning =>true, :fallbacks_for_empty_translations => true
