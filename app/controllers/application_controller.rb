8aa52bc83c8f655f24689763c9d0c607e8fe4e1a
Andreas Stephanides <andreas.stephanides@gmail.com>
2014-11-08 10:30:01 +0100

    redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,15 @@
       end
     end
   end
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_path(:only_path => false, :protocol => 'http')
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end^
 def get_theme
   if ThemesForRails.available_theme_names.include?(params[:theme])
   params[:theme]
