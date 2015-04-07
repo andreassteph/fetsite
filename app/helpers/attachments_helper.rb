0df5e72e1597a9968b2d109f3b5dbcdd499ca3b9
Andreas Stephanides <andreas.stephanides@gmail.com>
2015-04-07 23:21:33 +0200

    attachment new version

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,9 @@
 module AttachmentsHelper
+
+  def render_attachments_for(p)
+    a= Attachment.new 
+    a.parent=p
+    render(partial:"attachments/attachment_list", object: p.attachments, locals: {editor: (can?(:edit, p)), parent: p} )+ ((can?(:edit, p))? (render partial:"attachments/form_bulk2", object: a ): "")
+    
+  end
 end
