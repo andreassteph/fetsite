d146cf09a735698c6bdfd274a26b26f711a22744
Andreas Stephanides <andreas.stephanides@gmail.com>
2015-06-09 12:03:27 +0200

    AutoCommit Die Jun  9 12:03:27 CEST 2015

--- app/helpers/neuigkeiten_helper.rb
+++ app/helpers/neuigkeiten_helper.rb
@@ -1,5 +1,5 @@
 module NeuigkeitenHelper # :nodoc:
   def send_daily_newsletter(user)
-   
+    NewsMailer.daily_newsletter(user.id).deliver
   end
 end
--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -6,6 +6,9 @@ class NewsMailer < ActionMailer::Base
     mail(to: email, subject: @neuigkeit.title)
   end
   def daily_newsletter(user_id)
-    
+    user=User.find(user_id)
+    ability= Ability.new(user)
+    @neuigkeiten=Neuigkeit.accessible_by(ability).where(:cache_order<2)
+
   end
 end
