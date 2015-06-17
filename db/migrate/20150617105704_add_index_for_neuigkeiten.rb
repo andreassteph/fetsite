815c81add0c755027417806336009e3237d15431
Andreas Stephanides <andreas.stephanides@gmail.com>
2015-06-17 14:03:07 +0200

    AutoCommit Mit Jun 17 14:03:07 CEST 2015

--- db/migrate/20150617105704_add_index_for_neuigkeiten.rb
+++ db/migrate/20150617105704_add_index_for_neuigkeiten.rb
@@ -1,12 +1,11 @@
 class AddIndexForNeuigkeiten < ActiveRecord::Migration
-  def up
+  def change
     add_index :neuigkeiten, :rubrik_id
+    add_index :neuigkeiten, :cache_is_published
     add_index :calentries, [:object_type, :object_id]
     add_index :attachments, [:parent_type, :parent_id]
     add_index :attachments, [:flag_titlepic, :parent_type, :parent_id]
-    add_index :neuigkeit_translations, :neuigkeit_id 
+    add_index :rubriken, :public
   end
 
-  def down
-  end
 end
