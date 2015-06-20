99e32424c70251d23a96f589b2f50dd4d19bbd6f
Andreas Stephanides <andreas.stephanides@gmail.com>
2015-06-20 15:03:01 +0200

    AutoCommit Sam Jun 20 15:03:01 CEST 2015

--- lib/flagable.rb
+++ lib/flagable.rb
@@ -21,7 +21,7 @@ module Flagable
         end
         respond_to do |format|
           format.html {render :text=>@obj.to_yaml}
-          format.js {render :text => "alert('#{lflag.to_s} #{ ActionController::Base.helpers.escape_javascript(@obj.to_yaml.to_s)}')"}
+          format.js {render :text => "$(\"#{@obj.flaglinkid(params[:flag])}\").replaceWith("+ActionController::Base.helpers.escape_javascript(ActionController::Base.helpers.flag_link(@obj,params[:flag],ActionController::Base.helpers.content_tag("i", "", class:"fa fa-flag")))+ "); alert('#{lflag.to_s} #{ ActionController::Base.helpers.escape_javascript(@obj.to_yaml.to_s)}');"}
         end
       end
     end
@@ -44,16 +44,18 @@ module Flagable
         v= false if v.nil?
         v
       end
-      def flagtagid(flag)
+      def flaglinkid(flag)
         return self.class.to_s + "_" + self.id.to_s + "_flag_"+flag.to_s
       end
     end
   end
   module FlagableHelper
-    def flag_link(obj, flag, value=false)
+
+    def flag_link(obj, flag, text)
       value=obj.get_flag(flag)
       color=(value) ? "red" :"grey"
-link_to fa_icon("flag"), flag_beispiel_path(obj,{flag: flag, value: !value}), remote: true, style:("color:" +color )
+link_to text, Rails.application.routes.url_helpers.flag_beispiel_path(obj,{flag: flag, value: !value, theme: nil, locale: nil}), remote: true, style:("color:" +color ), id: obj.flaglinkid(flag)
     end
   end
 end
+ActionView::Base.send :include, Flagable::FlagableHelper
